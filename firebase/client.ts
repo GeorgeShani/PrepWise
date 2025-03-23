039094c3e6291b1eed1617df999089766bdc60b7
GeorgeShani <shanidze.geo2004@gmail.com>
2025-03-23 20:46:42 +0400

    Minor changes in Firebase client configuration

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBO6xcxY-hZxsRhehvR2Uwva0k-nVqH_rA",
-  authDomain: "prepwise-f9d0a.firebaseapp.com",
-  projectId: "prepwise-f9d0a",
-  storageBucket: "prepwise-f9d0a.firebasestorage.app",
-  messagingSenderId: "437605342419",
-  appId: "1:437605342419:web:81a685ce4ce2a5332a8826",
-  measurementId: "G-W04FND2HWY",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
